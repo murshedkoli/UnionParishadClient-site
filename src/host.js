70144cfd892bff89fd1c05191ec96952483dfe80
murshedkoli <murshedkoli@gmail.com>
2023-02-01 22:59:15 +0600

    update

--- src/host.js
+++ src/host.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const host = "https://unionparishadservice-server-site-production.up.railway.app";
-=======
-export const host = "http://localhost:5000";
->>>>>>> ee7e717f477171b88d8aa3d79dc6c1c66b8eede4
